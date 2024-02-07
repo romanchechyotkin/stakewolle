8657f67beb4684c288e742c71fff199d67db7baf
romanchechyotkin <romanchechyotkin@gmail.com>
2024-02-07 22:07:14 +0300

    linting

--- alembic/versions/1580f0606fb8_users.py
+++ alembic/versions/1580f0606fb8_users.py
@@ -7,9 +7,9 @@ Create Date: 2024-02-07 21:58:33.970961
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '1580f0606fb8'
